ae567caf97439f1bd29e42565afd4fa1f5c86f0e
AJ <ahumphries102@gmail.com>
2019-04-02 15:03:02 -0500

    working on divs to make them slide correctly

--- src/Components/Animated.js
+++ src/Components/Animated.js
@@ -7,12 +7,10 @@ export default function Animated(props){
     zIndex:'1', 
     from: { right: '0%' }, 
     right: props.toggle ? '0%' : '50%', 
-    config: { duration: 1000 } 
+    config: { duration: 1000} 
   })
   return (
-    <section >
-      <animated.div style={slide} className=""> <img src="https://s.abcnews.com/images/US/gale3-ht-ml-190326_hpMain_16x9_992.jpg" alt="dog"/><img src="https://s.abcnews.com/images/US/gale3-ht-ml-190326_hpMain_16x9_992.jpg" alt="dog"/>
+      <animated.div className="imgFlex"> <img src="https://s.abcnews.com/images/US/gale3-ht-ml-190326_hpMain_16x9_992.jpg" alt="dog"/><img src="https://s.abcnews.com/images/US/gale3-ht-ml-190326_hpMain_16x9_992.jpg" alt="dog"/>
       </animated.div>
-    </section>
   )
 }
--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -5,39 +5,36 @@ import Animated from './Animated'
 import Animated2 from './Animated2'
 
 export default function MainContainer(){
+	
+
   const [toggle, setToggle] = useState(true)
   const [button, setButton] = useState('>')
-
+  
   function toggleImg(){
   	return setToggle(!toggle)
   }
 
   function toggleButton(){
-  	return setButton(button === 'x' ? '>' : 'x')
+  	setButton(button === 'x' ? '>' : 'x')
   }
 
   function callTwoFunctions(){
   	toggleImg() 
   	toggleButton()
+  	console.log(button)
   }
 	return(
 		<section className="mainContainer">
-			<div className="imgWrapper">
-				<div className="placeHoldL">
-					<div className="wrapL">
-						<div className="introText">
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-						</div>
-					</div>
-				</div>
-				<div className="wrapR">
-					<button onClick={callTwoFunctions} className="toggleSlide">{button}</button>
-					<div className="imgCon">
-						<Animated toggle={toggle}/>
-						<Animated2 toggle={toggle}/>
-					</div>
-				</div>
+			<div className="about" id="about"></div>
+			<div className="wrapL" id="">
+				<nav className="nav">
+					<a href="#about">about</a>
+					<a href="#home">work</a>
+				</nav>
 			</div>
+			<div className="wrapR" id="home">
+			</div>
+			<div className="port"></div>
 		</section>
 	)
 }
