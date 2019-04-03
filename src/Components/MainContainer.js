adf2ca177840e87c46ae22f9f307877a35f3754d
AJ <ahumphries102@gmail.com>
2019-04-03 13:56:46 -0500

    Latest Iteration of Portfolio

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -5,36 +5,52 @@ import Animated from './Animated'
 import Animated2 from './Animated2'
 
 export default function MainContainer(){
-	
+	const [toggleAbout, setToggleAbout] = useState(true)
+	const [toggleWork, setToggleWork] = useState(true)
+	const [button, setButton] = useState('>')
+	const [shiftCon, setShiftCon] = useState(true)
+	const [navAbout, setNavAbout] = useState('about')
+	const [navWork, setNavWork] = useState('work')
 
-  const [toggle, setToggle] = useState(true)
-  const [button, setButton] = useState('>')
-  
-  function toggleImg(){
-  	return setToggle(!toggle)
-  }
+	const togWork = useSpring({
+	    zIndex:'1', 
+	    from: { transform: 'translate(0%,0)' }, 
+	    transform:`translate: (${toggleWork ? '0%' : '-25%'},0)`, 
+	    config: { ease: 5000 } 
+  	})
 
-  function toggleButton(){
-  	setButton(button === 'x' ? '>' : 'x')
-  }
-
-  function callTwoFunctions(){
-  	toggleImg() 
-  	toggleButton()
-  	console.log(button)
-  }
+	const togAbout = useSpring({
+	    zIndex:'1', 
+	    from: { transform: 'translate(0%,0)' }, 
+	    transform:`translate: (${toggleAbout ? '0%' : '25%'},0)`, 
+	    config: { ease: 5000 } 
+  	})
+	function toggleButton(){
+		setButton(button === 'x' ? '>' : 'x')
+	}
+	function showAbout(){
+		setNavAbout(navAbout === 'about' ? 'home' : 'about')
+		setToggleAbout(!toggleAbout)
+		console.log(toggleWork)
+	}
+	function showWork(){
+		setNavWork(navWork === 'work' ? 'home' : 'work')
+		setToggleWork(!toggleWork)
+	}
 	return(
-		<section className="mainContainer">
+		<div>
+			<nav className="nav">
+				<a href="#" onClick={showAbout}>{navAbout}</a>
+				<a href="#" onClick={showWork}>{navWork}</a>
+			</nav>
+		<animated.div style={togAbout, togWork} className="mainContainer" id="mainCon">
 			<div className="about" id="about"></div>
 			<div className="wrapL" id="">
-				<nav className="nav">
-					<a href="#about">about</a>
-					<a href="#home">work</a>
-				</nav>
 			</div>
-			<div className="wrapR" id="home">
+			<div className="wrapR">
 			</div>
-			<div className="port"></div>
-		</section>
+			<div className="port" id="home"></div>
+		</animated.div>
+		</div>
 	)
 }
