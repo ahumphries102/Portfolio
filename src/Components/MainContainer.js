f32678cc1f8e897bafa2dae36308122366a97319
AJ <ahumphries102@gmail.com>
2019-04-05 15:37:17 -0500

    scroll in progress phase2

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -11,7 +11,12 @@ export default function MainContainer(props){
 	const [button, setButton] = useState('>')
 	const [navAbout, setNavAbout] = useState('about')
 	const [navWork, setNavWork] = useState('work')
-	const [height, setHeight] = useState('50%')
+	const [inc, setInc] = useState(0)
+	const [currentInc, setCurrentInc] = useState(0)
+	// set the height of psuedo scroll bar in center of screen
+	const [height, setHeight] = useState(0)
+
+
 
 	const slide = useSpring({
 	    zIndex:'1', 
@@ -21,7 +26,7 @@ export default function MainContainer(props){
   	})
   	const heightInc = useSpring({
   		background:'red', 
-	    height: height
+	    height: `${height}%`
   	})
 	function show(view){
 		if(view === 'about' ){
@@ -29,7 +34,6 @@ export default function MainContainer(props){
 			setNavPos('25%')
 			setNavWork(navWork === 'work' ? '': 'work')
 			setNavAbout(navAbout === 'about' ? 'home': 'about')
-			console.log('we are viewing', view)
 		}
 		else if(view === 'work'){
 			setToggleWork(!toggleWork)
@@ -37,19 +41,34 @@ export default function MainContainer(props){
 			setButton(button === '>' ? 'x': '>')
 			setNavWork(navWork === 'work' ? 'home': 'work')
 			setNavAbout(navAbout === 'about' ? '': 'about')
-			console.log('we are viewing', view)
 		}
 		else{
 			//in the nav bar set text to home or work
 			setToggleWork(!toggleWork)
 		}
 	}
+	function increaseHeight(){
+		
+		const scrolling = document.getElementById('wrapR').scrollTop.toFixed()
+
+		if( scrolling < currentInc){
+			setHeight(height - .5)
+		}
+		else{
+			setHeight(height + .5)
+		}
+
+		const scrolled = document.getElementById('wrapR').scrollTop.toFixed()
+
+		setCurrentInc(scrolled)
+		console.log((scrolled / 100) * 10)
+	}
 	return(
 		<animated.div style={slide} className="mainContainer" id="mainCon">
 				<div className="about" id="about">
 				</div>
 				<div className="wrapL" id="">
-				<animated.div style={heightInc} onScroll={console.log('hey')} className="scrollFill"></animated.div>
+				<animated.div style={heightInc}  className="scrollFill"></animated.div>
 					<ul className='scrollList'>
 						<li>
 						</li>
@@ -76,7 +95,7 @@ export default function MainContainer(props){
 						<a href="#about" onClick={()=>show('about')}>{navAbout}</a>
 					</nav>
 				</div>
-				<div onClick={()=>show('work')} className="wrapR">
+				<div onClick={()=>show('work')} onScroll={()=>increaseHeight()} className="wrapR" id="wrapR" >
 				<img src={images.img1}/>
 				<img src={images.img2}/>
 					<button className="toggleSlide">{button}</button>
