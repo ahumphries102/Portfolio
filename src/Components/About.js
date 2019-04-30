9de12e95f5d23bd693ea82b6a1287f7e991179c1
AJ <ahumphries102@gmail.com>
2019-04-30 17:12:02 -0500

    Added social media icons

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,6 +8,11 @@ export default function About(){
 				<p>I've created <span>logos, web applications</span> and <span>advertisements</span> for the city of Arlington, Washington and many other clients.</p>
 				<p>Hire me at <span id="email"><a href = "mailto: ahumphries102@gmail.com?subject=Portfolio">Ahumphries102@gmail.com</a></span></p>
 			</div>
+			<section className="socialMedia">
+			<a href="https://www.linkedin.com/in/ahumphriesprofile/"><img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/56-linkedin-512.png"/></a>
+			<a href="https://github.com/ahumphries102?tab=repositories"><img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/71-github-512.png"/></a>
+
+			</section>
 		</section>
 	)
 }
