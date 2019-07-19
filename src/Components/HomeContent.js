993fe2cc26395ade27a3adb5403d851c312fff03
AJ <ahumphries102@gmail.com>
2019-07-19 14:18:16 -0500

    Updated project 2 with a link to hosted project

--- src/Components/HomeContent.js
+++ src/Components/HomeContent.js
@@ -6,7 +6,7 @@ export default function Home(props){
 	return(
 		<section className="home">
 				<h1><span>A</span>ndrew <span><br/>H</span>umphries</h1>
-				
+				<h2>Graphic Designer <span>/</span> Software Engineer</h2>
 		</section>
 	)
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -17,7 +17,7 @@ export default function Portfolio() {
             <figcaption>Overall view of my capstone which uses Material.ui</figcaption>
             <img src={Images.img3} alt="placeholder"/>
     		<figcaption>Backend code using Flask fetching an API using Async/Await</figcaption>
-    		<img src={Images.img4} alt="placeholder"/>
+            <img src={Images.img4} alt="placeholder"/>
             <figcaption>A database I created to hold all the relevant information</figcaption>
     	</div>
     	
@@ -25,12 +25,15 @@ export default function Portfolio() {
             <div className="portText">
         		<h2>Magic the Gathering Deck Builder</h2>
                 <p>Users can select cards to build their deck. The bar on the right reflects the users choice. They may delete unwanted cards and once they're satisfied with their deck they can click save to save their deck to an SQL database.</p>
-                <a href="https://github.com/ahumphries102/database_project">Github</a>
+                <a href="https://github.com/ahumphries102/database_project">Github</a><br/>
+                <a href="https://ahumphries102.github.io/deckbuilder/" target="_blank" rel="noopener noreferrer">View Project</a>
             </div>
             <img src={Images.img5} alt="placeholder"/>
             <figcaption>Node.js is used along with an SQL script to save all the users chosen cards to a database</figcaption>
             <img src={Images.img6} alt="placeholder"/>
             <figcaption>All choices a user makes will be displayed on the right handside</figcaption>
+            <img src={Images.img4} alt="placeholder"/>
+            <figcaption>A database I created to hold all the relevant information</figcaption>
         </div>
         
     	<div className="portInfo">
