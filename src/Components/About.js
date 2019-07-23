14a15b2225310d33835d548665901b5d1b918fac
AJ <ahumphries102@gmail.com>
2019-07-23 16:27:39 -0500

    Updated navbar to include portfolio git page

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ export default function About(){
 			<div className="aboutText">
 				<h2>I’m a results driven web developer and graphic designer who clearly and consistently turns ideas into visuals and interactivity.</h2>
 				<p>I've had an exciting time learning everything I can about Web Development beginning when I was in college. From there I joined a company and built their entire web application. Since then I got my certificate at General Assembly in Web Development where I studied to be a full stack developer.</p>
-				<p>Hire me at</p> <a href = "mailto: ahumphries102@gmail.com?subject=Portfolio"><span id="email">Ahumphries102@gmail.com</span></a>
+				<p>Learn More About me</p> <a href = "mailto: ahumphries102@gmail.com?subject=Portfolio"><span id="email">Ahumphries102@gmail.com</span></a>
 			</div>
 			<section className="socialMedia">
 			<a href="https://www.linkedin.com/in/ahumphriesprofile/"><img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/56-linkedin-512.png" alt="linkedin icon"/></a>
